12923795798e310359bdea4d1225bce94acb6e40
Monishparameswaran <monishparameshwaran@gmail.com>
2024-03-15 20:50:33 +0530

    Added and updated booking logic

---
+++ src/Bus.java
@@ -0,0 +1,140 @@
+import java.util.List;
+import java.util.Scanner;
+public class Bus implements AdminInterface{
+    private int id ;
+    private int seatNo=333;
+    private static int totCapacity;
+    private  int capacity;
+    private  int pricePerTicket;
+    private boolean AC;
+    private int profitPerBus;
+    private static int totalEarnings;
+    private static final Scanner in;
+    static{
+        in=new Scanner(System.in);
+    }
+    Bus(List<Bus>buses){
+       super();
+       addBus(buses);
+    }
+    public  void addBus(List<Bus>buses){
+        System.out.print("Please enter Id of new Bus: ");
+        setName(in.nextInt());
+        System.out.println();
+        System.out.print("Enter capacity of Bus: ");
+        setCapacity(in.nextInt());
+        setTotCapacity(getCapacity());
+        System.out.println();
+        System.out.print("is it AC : (True or False) :");
+        setAC(in.nextBoolean());
+        System.out.println();
+        System.out.print("Enter price per ticket: ");
+        setPricePerTicket(in.nextInt());
+        buses.add(this);
+    }
+
+    public int getProfitPerBus() {
+        return profitPerBus;
+    }
+
+    public void setProfitPerBus(int profitPerBus) {
+        this.profitPerBus = profitPerBus;
+    }
+
+    public static void removeBus(List<Bus>buses){
+        System.out.print("Enter bus id: ");
+        int id=in.nextInt();
+        for(Bus b:buses){
+            if(b.getName()==id){
+                buses.remove(b);
+                System.out.println("Bus removed Successfully");
+                break;
+            }
+        }
+    }
+
+    public static int getTotCapacity() {
+        return totCapacity;
+    }
+
+    public static void setTotCapacity(int totCapacity) {
+        Bus.totCapacity = totCapacity;
+    }
+
+    public static int getTotalEarnings() {
+        return totalEarnings;
+    }
+
+    public static void setTotalEarnings(int totalEarnings) {
+        Bus.totalEarnings = totalEarnings;
+    }
+
+    public static void generateReport(List<Bus>buses) {
+        System.out.println("-----------------Bus REPORT----------------------------");
+        for(Bus b:buses){
+            b.setProfitPerBus((getTotCapacity()-b.getCapacity())*b.getPricePerTicket());
+            setTotalEarnings(getTotalEarnings()+b.getProfitPerBus());
+            System.out.println("--------------------------------------------");
+            System.out.println("Bus no: "+b.getName());
+            System.out.println("Earnings: "+b.getProfitPerBus());
+            System.out.println("Empty Seats: "+b.getCapacity());
+            System.out.println("-------------------------------------------");
+
+        }
+        System.out.println("Total Earnings: "+ Bus.getTotalEarnings());
+        System.out.println();
+        System.out.println("--------------------------------------------------");
+    }
+
+    public int getName() {
+        return id;
+    }
+
+    public int getPricePerTicket() {
+        return pricePerTicket;
+    }
+
+    public void setPricePerTicket(int pricePerTicket) {
+        this.pricePerTicket = pricePerTicket;
+    }
+
+    public void setName(int id){
+        this.id =id;
+    }
+    public  int getCapacity() {
+        return capacity;
+    }
+
+    public int getSeatNo() {
+        return seatNo;
+    }
+
+    public void setSeatNo(int seatNo) {
+        this.seatNo = seatNo;
+    }
+
+    public  void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean getAC() {
+        return AC;
+    }
+
+    public void setAC(boolean AC) {
+        this.AC = AC;
+    }
+    public static void getBusDetails(List<Bus>bus){
+        System.out.println("----------------------------------");
+        for(Bus b:bus){
+            System.out.println("Bus no: "+b.getName());
+            System.out.println("Capacity: "+b.getCapacity());
+            System.out.println("PricePerTicket: "+b.getPricePerTicket());
+            System.out.println("AC: "+b.getAC());
+            System.out.println();
+        }
+        System.out.println("----------------------------------");
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,99 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class MyPair {
+    String name;
+    String pass;
+    MyPair(String a,String b){
+        this.name=a;
+        this.pass=b;
+    }
+    @Override
+    public boolean equals(Object obj){
+        if(obj==null)return false;
+        if(obj==this)return true;
+        MyPair myobj=(MyPair)obj;
+        return myobj.name.equals(this.name) && myobj.pass.equals(this.pass);
+    }
+
+}
+public class Main {
+    private static boolean validateADMIN(List<MyPair>adminDB){
+        Scanner in=new Scanner(System.in);
+        System.out.println();
+        System.out.println("-------------Admin Login-------------");
+        System.out.print("Enter Name: ");
+        String name=in.next();
+        System.out.print("Enter Password: ");
+        String pass=in.next();
+        System.out.println("-----------------------------------");
+        System.out.println();
+        if(!adminDB.contains(new MyPair(name,pass)))return false;
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+
+        List<Bus>bus=new ArrayList<>();
+        List<Booking>bookings=new ArrayList<>();
+        List<MyPair>adminDB=new ArrayList<>();
+        Scanner in=new Scanner(System.in);
+        adminDB.add(new MyPair("Monish","1234"));
+        int userOption=-2;
+        while(true){
+            System.out.println("enter 0 for Admin: ");
+            System.out.println("enter 1 to getBusInfo: ");
+            System.out.println("enter 2 to BookTickets: ");
+            System.out.println("enter 3 to CancelTickets: ");
+
+            System.out.println("enter -1 to Exit : ");
+            try{
+                userOption=in.nextInt();
+            }
+           catch(Exception e){
+                System.out.println("Please enter Integer value , occured: ");
+               System.out.println("Reenter value: ");
+               in.next();
+               in.next();
+                userOption=in.nextInt();
+           }
+            if(userOption==0){
+                if(validateADMIN(adminDB)){
+                    System.out.println("Welcome Back Admin!");
+                    while(true){
+
+                        System.out.println();
+                        System.out.println("========ADMIN===========");
+                        System.out.println("enter 0 to Add Buses");
+                        System.out.println("enter 1 to remove Buses");
+                        System.out.println("enter 2 to Generate report of Buses");
+                        System.out.println("enter -1 to Exit");
+
+                        int adminOption=in.nextInt();
+                        if(adminOption==0)new Bus(bus);
+                        if(adminOption==1)Bus.removeBus(bus);
+                        if(adminOption==2)Bus.generateReport(bus);
+                        if (adminOption==-1)break;
+                    }
+                }
+                else{
+                    System.out.println("Invalid Admin Credentials");
+                }
+            }
+            if(userOption==1)Bus.getBusDetails(bus);
+            if(userOption==2)new Booking(bus,bookings);
+            if(userOption==3){
+                System.out.print("Please enter ticket ID: ");
+                Booking.cancelBusTickets(in.nextInt(),bus,bookings);
+            }
+            if(userOption==4)break;
+
+        }
+    }
+}
